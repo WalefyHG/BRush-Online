b1b2fcde596b5ec5fab9c6ebd5f38e5b8107e905
unknown <walefylima@gmail.com>
2024-08-21 14:52:23 -0300

    Gerando

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/BRush-Online',
+  base: '/BRush-Online/',
   build: {
     chunkSizeWarningLimit: 1000,
-    outDir: 'build',
   }
 })
